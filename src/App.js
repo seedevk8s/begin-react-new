1363bb9d35b6cb04d30bf0a2b0e27b84508f699a
chjin <seedevk8s@gmail.com>
2020-01-05 19:42:03 +0900

    jsx에 스타일 적용, 값 표현

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React from 'react';
 import Hello from "./Hello";
 
 function App() {
-  return (
-    <div>
-      <Hello/>
-    </div>
-  );
+    const name = 'react 알아보자!';
+    const style = {
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: 24,
+        padding:'1rem'
+    };
+      return (
+        <>
+            <Hello/>
+            <div style={style}>{name}</div>
+        </>
+      )
 }
 
 export default App;
+
+
+
+
+
+
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Hello() {
-    return <div>안녕하세요. 1월 5일 입니다!</div>;
+    return <div>안녕하세요. 1월 5일 입니다!<button>버튼</button></div>;
 }
 
 export default Hello;
