adfae5f4d590205509aa52db666ea0c580eb7c89
chjin <seedevk8s@gmail.com>
2020-01-05 21:39:32 +0900

    App에 있는 내용이 Wrapper에서 보여지고 있지 않음.<==children props로 받아와서 보여주고 있음. <--isSpecial 값을 주지않을때는 기본값으로 true가 설정됨

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Wrapper from "./Wrapper";
 function App() {
       return (
         <Wrapper>
-          <Hello name="react!" color="red" isSpecial={true}/>  {/* name, color가 props임*/}
+          <Hello name="react!" color="red" isSpecial/>  {/* name, color가 props임*/}
           <Hello color="pink"/>
         </Wrapper>
       )
