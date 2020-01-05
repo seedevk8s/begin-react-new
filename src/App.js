010549884597117f275afae4526380b74dd50368
chjin <seedevk8s@gmail.com>
2020-01-05 20:49:57 +0900

    재정비함--원소스

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import Hello from "./Hello";
-import './App.css'
 
 function App() {
-    const name = 'react 알아보자!';
-    const style = {
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: 24,
-        padding:'1rem'
-    };
       return (
-        <>
-            {/* 주석이므로 브라우저에 나타나지 않음 */}
-            <Hello
-                // 이런식으로 작성하는 주석문은 화면에 안나타남.
-            />
-            <div
-                // 이런식으로 작성하는 주석문은 화면에 안나타남.
-                style={style}>{name}</div>
-            <div className="gray-box"></div>
-        </>
+          <Hello name="react!"/>
       )
 }
 
