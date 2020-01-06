8bcdb3995f4d6c5f0d8b49adaa6ed6c6ca019d0d
D191375 <seedevk8s@gmail.com>
2020-01-06 11:48:05 +0900

    Count 컴포넌트--함수 2개 추가

--- src/Count.js
+++ src/Count.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 function Counter() {
+    const onIncrease = () => {
+        console.log('+1');
+    }
+    const onDecrease = () => {
+        console.log('-1');
+    }
+
     return (
         <div>
             <h1>0</h1>
