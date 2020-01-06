9f3fa607c5cf5f1136c3e8635195084183e3d1fe
chjin <seedevk8s@gmail.com>
2020-01-06 22:45:57 +0900

    InputSample 컴포넌트 - onChange 이벤트 추가 <== input 에 onChange 이벤트 연결. <-- 상태값 확인하기 위한 useState 추가

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function InputSample() {
+    const [text, setText] = useState('');
+
     const onChange = (e) => {
-        console.log(e.target);
+        console.log(e.target.value);
     }
 
     return (
