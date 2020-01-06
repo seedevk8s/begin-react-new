c3d330f5e5a961700b886580ff8b6814f5968905
D191375 <seedevk8s@gmail.com>
2020-01-06 11:50:33 +0900

    Count 컴포넌트--함수 2개와 버튼에 이벤트 연결

--- src/Count.js
+++ src/Count.js
@@ -11,8 +11,8 @@ function Counter() {
     return (
         <div>
             <h1>0</h1>
-            <button>+1</button>
-            <button>-1</button>
+            <button onClick={onIncrease}>+1</button>
+            <button onClick={onDecrease}>-1</button>
         </div>
     );
 }
