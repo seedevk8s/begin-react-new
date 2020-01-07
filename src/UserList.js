c762ce3a3773df9c04f57f70a5e370211b8d000f
D191375 <seedevk8s@gmail.com>
2020-01-07 10:40:35 +0900

    한파일에서 두개이상의 컴포넌트를 만들어도 됨. 그래서 User를 추가함

--- src/UserList.js
+++ src/UserList.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+
+function User({ user }) {
+    return (
+        <div>
+            <b>{user.username}</b> <span>({user.email})</span>
+        </div>
+    );
+}
+
 function UserList() {
     const users = [
         {
