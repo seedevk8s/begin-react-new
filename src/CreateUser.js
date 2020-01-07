bc593836dd0ae276a267ec8d76b3a5de1ec0b220
D191375 <seedevk8s@gmail.com>
2020-01-07 11:33:59 +0900

    CreateUser 함수에서는 총 4개의 pops를 가지고 올것임.<-- JSX 값을 바로 반환해 줄것임. <--버튼에 onClick 이벤트 연결함.

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -7,9 +7,15 @@ function CreateUser({ username, email, onChange, onCreate}) {
                 name="username"
                 placeholder="계정명"
                 onChange={onChange}
-                value={username}/>
-            <input/>
-            <button>등록</button>
+                value={username}
+            />
+            <input
+                name="email"
+                placeholder="이메일"
+                onChange={onChange}
+                value={email}
+            />
+            <button onClick={onCreate}>등록</button>
         </div>
     );
 }
