340434821140d4d2e50e66fb443eafdc6e263123
D191375 <seedevk8s@gmail.com>
2020-01-07 15:13:13 +0900

    삭제 버튼에 클릭 이벤트 연결, 아직 onRemove()는 구현 전임. -- user로 비구조적기법으로 중복되는것을 하나로 묶어서 표현함.

--- src/UserList.js
+++ src/UserList.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 
 function User({ user, onRemove }) {
+    const { username, email, id } = user;
+
     return (
         <div>
-            <b>{user.username}</b> <span>({user.email})</span>
-            <button onClick={() => onRemove(user.id)}>삭제</button>
+            <b>{username}</b> <span>({email})</span>
+            <button onClick={() => onRemove(id)}>삭제</button>
         </div>
     );
 }
