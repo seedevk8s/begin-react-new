40fbe735528c2767d71bbad3aa728e435727f2e9
D191375 <seedevk8s@gmail.com>
2020-01-07 10:34:30 +0900

    배열안에 3개의 객체를 구현 -- 비효율적인 구현 방법임 <--배열안에 있는 객체를 랜더링하는 것을 작성. <--컴포넌트를 배포함.

--- src/UserList.js
+++ src/UserList.js
@@ -19,6 +19,19 @@ function UserList() {
         }
     ];
 
-
-
+    return (
+        <div>
+            <div>
+                <b>{users[0].username}</b> <span>({users[0].email})</span>
+            </div>
+            <div>
+                <b>{users[1].username}</b> <span>({users[1].email})</span>
+            </div>
+            <div>
+                <b>{users[2].username}</b> <span>({users[2].email})</span>
+            </div>
+        </div>
+    );
 }
+
+export default UserList;
