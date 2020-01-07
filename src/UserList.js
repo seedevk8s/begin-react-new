60e76102cb8960668a6b9898cfbcd3863f824e39
D191375 <seedevk8s@gmail.com>
2020-01-07 10:57:07 +0900

    방금전의 경우는 정적인 갯수의 배열을 표시한것임<--이제 동적갯수의 배열의 경우를 표시할것임--map()을 사용하면 됨. <--User라는 컴포넌트를 활용하여 props를 출력함. <--key에 해당하는 것을 추가해줌(콘솔상 경고 사라짐)

--- src/UserList.js
+++ src/UserList.js
@@ -32,7 +32,7 @@ function UserList() {
         <div>
             {
                 users.map(
-                    user => (<User user={user}/>)
+                    user => (<User user={user} key={user.id}/>)
                 )
             }
         </div>
