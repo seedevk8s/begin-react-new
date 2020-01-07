9f024b721752de17f6974890e62c468ec455a068
D191375 <seedevk8s@gmail.com>
2020-01-07 10:22:59 +0900

    배열안에 3개의 객체를 구현 -- 비효율적인 구현 방법임

---
+++ src/Sample.js
@@ -0,0 +1,21 @@
+const users = [
+    {
+        id:1,
+        username: 'veloper',
+        email: 'public.veloper@gmail.com'
+    },
+    {
+        id: 2,
+        username: 'tester',
+        email: 'tester@example.com'
+    },
+    {
+        id: 3,
+        username: 'liz',
+        email: 'liz@example.com'
+    }
+];
+
+
+
+
