2678cada1d7e0c18b1608850de1a85d3cc231733
D191375 <seedevk8s@gmail.com>
2020-01-08 10:52:05 +0900

    onChange를 useCallback()으로 감쌈. inputs가 바뀔때만 onChange 함수를 호출하고, 그외에는 재사용함.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo } from 'react';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 
@@ -14,13 +14,13 @@ function App() {
     });
 
     const { username, email } = inputs;
-    const onChange = e => {
+    const onChange = useCallback(e => {
         const { name, value } = e.target;
         setInputs({
             ...inputs,
             [name]: value
         });
-    };
+    }, [inputs]);
 
     const [users, setUsers ] = useState([
         {
