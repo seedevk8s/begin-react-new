10e9992d2661a2754a09546af3d7fcda49742ad8
D191375 <seedevk8s@gmail.com>
2020-01-08 13:35:50 +0900

    App 컴포넌트에 생성했던 객체들 삭제

--- src/App.js
+++ src/App.js
@@ -35,72 +35,7 @@ const initialState = {
 };
 
 function App() {
-    const [inputs, setInputs] = useState({
-       username: '',
-       email: ''
-    });
 
-    const { username, email } = inputs;
-    const onChange = useCallback(e => {
-        const { name, value } = e.target;
-        setInputs({
-            ...inputs,
-            [name]: value
-        });
-    }, [inputs]);
-
-    const [users, setUsers ] = useState([
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.veloper@gmail.com',
-            active: true,
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com',
-            active: false,
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com',
-            active: false,
-        }
-    ]);
-
-    const nextId = useRef(4);
-
-    const onCreate = useCallback(() => {
-        const user = {
-          id: nextId.current,
-          username,
-          email
-        };
-        setUsers(users => users.concat(user));
-
-        setInputs({
-           username: '',
-           email: ''
-        });
-
-        nextId.current += 1;
-    }, [username,email]);
-
-    const onRemove = useCallback(id => {
-        setUsers(users => users.filter(user => user.id !== id));
-    }, []);
-
-    const onToggle = useCallback(id => {
-        setUsers(users => users.map(
-            user => user.id === id
-                ? {...user, active: !user.active}
-                : user
-        ));
-    }, []);
-
-    const count = useMemo(() => CountActiveUsers(users), [users]);
 
       return (
         <>
